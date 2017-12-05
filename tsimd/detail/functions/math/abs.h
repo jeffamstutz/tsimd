b98824338d78ab8c329839da66e41fa4a1e4cbf4
Jefferson Amstutz <jefferson.d.amstutz@intel.com>
2017-12-05 10:51:28 -0600

    implement vectorized abs() via intrinsics

--- tsimd/detail/functions/math/abs.h
+++ tsimd/detail/functions/math/abs.h
@@ -30,18 +30,86 @@
 
 namespace tsimd {
 
-  template <typename T, int W>
-  TSIMD_INLINE pack<T, W> abs(const pack<T, W> &p)
-  {
-    pack<T, W> result;
+  // 1-wide //
 
-#if TSIMD_USE_OPENMP
-#  pragma omp simd
-#endif
-    for (int i = 0; i < W; ++i)
+  template <typename T>
+  TSIMD_INLINE pack<T, 1> abs(const pack<T, 1> &p)
+  {
+    return pack<T, 1>(std::abs(p[0]));
+  }
+
+  // 4-wide //
+
+  TSIMD_INLINE vfloat4 abs(const vfloat4 &p)
+  {
+#if defined(__SSE__)
+    return _mm_and_ps(p, _mm_castsi128_ps(_mm_set1_epi32(0x7fffffff)));
+#else
+    vfloat4 result;
+
+    for (int i = 0; i < 4; ++i)
       result[i] = std::abs(p[i]);
 
     return result;
+#endif
+  }
+
+  TSIMD_INLINE vint4 abs(const vint4 &p)
+  {
+#if defined(__SSE__)
+    return _mm_abs_epi32(p);
+#else
+    vint4 result;
+
+    for (int i = 0; i < 4; ++i)
+      result[i] = std::abs(p[i]);
+
+    return result;
+#endif
+  }
+
+  // 8-wide //
+
+  TSIMD_INLINE vfloat8 abs(const vfloat8 &p)
+  {
+#if defined(__AVX2__) || defined(__AVX__)
+    return _mm256_and_ps(p, _mm256_castsi256_ps(_mm256_set1_epi32(0x7fffffff)));
+#else
+    return vfloat8(abs(vfloat4(p.vl)), abs(vfloat4(p.vh)));
+#endif
+  }
+
+  TSIMD_INLINE vint8 abs(const vint8 &p)
+  {
+#if defined(__AVX2__)
+    return _mm256_abs_epi32(p);
+#elif defined(__AVX__)
+    return vint8(_mm_abs_epi32(p.vl), _mm_abs_epi32(p.vh));
+#else
+    return vint8(abs(vint4(p.vl)), abs(vint4(p.vh)));
+#endif
+  }
+
+  // 16-wide //
+
+  TSIMD_INLINE vfloat16 abs(const vfloat16 &p)
+  {
+#if defined(__AVX512F__)
+    return _mm512_castsi512_ps(
+      _mm512_and_epi32(
+        _mm512_castps_si512(p),_mm512_set1_epi32(0x7FFFFFFF)));
+#else
+    return vfloat16(abs(vfloat8(p.vl)), abs(vfloat8(p.vh)));
+#endif
+  }
+
+  TSIMD_INLINE vint16 abs(const vint16 &p)
+  {
+#if defined(__AVX512F__)
+    return _mm512_abs_epi32(p);
+#else
+    return vint16(abs(vint8(p.vl)), abs(vint8(p.vh)));
+#endif
   }
 
 }  // namespace tsimd
