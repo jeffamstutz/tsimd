aa9c3ab5d0d4d23a8c019ca3223b9d76a52ac20a
Jefferson Amstutz <jefferson.d.amstutz@intel.com>
2017-12-13 11:49:42 -0600

    Update license info

--- tsimd/detail/operators/logic/disallow_and_or.h
+++ tsimd/detail/operators/logic/disallow_and_or.h
@@ -1,7 +1,7 @@
 // ========================================================================== //
 // The MIT License (MIT)                                                      //
 //                                                                            //
-// Copyright (c) 2017 Jefferson Amstutz                                       //
+// Copyright (c) 2017 Intel Corporation                                       //
 //                                                                            //
 // Permission is hereby granted, free of charge, to any person obtaining a    //
 // copy of this software and associated documentation files (the "Software"), //
